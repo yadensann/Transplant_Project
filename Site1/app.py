994749650d764361a68292ffd4a0f778dc9119b0
oyasumi <yadensann@gmail.com>
2021-03-25 22:04:42 -0700

    working on Flask app and mock HTML to test model

---
+++ Site1/app.py
@@ -0,0 +1,33 @@
+import flask
+import pickle
+
+# Use pickle to load in the pre-trained model.
+with open(f'model/survival_rate_model_xgboost.pkl', 'rb') as f:
+    model= pickle.load(f)
+    
+app = flask.Flask(__name__, template_folder= 'templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method =='GET':
+        return(flask.render_template('Survival.html'))
+    
+    if flask.request.method == 'POST':
+        organ = flask.request.form['organ']
+        donor_type = flask.request.form['donor type']
+        state = flask.request.form['state']
+        
+        input_variabes= pd.DataFrame([[organ, organ_type, state]],
+                            columns=['Organ', 'Donor_Type, State Abv'],
+                            dtype= float)
+        prediction= model.predict(input_variables)[0]
+        
+        return flask.render_template('Survival.html',
+                                    original_input= {'Temperature': temperature,
+                                                      'Humidity': humidity,
+                                                      'Windspeed': windspeed},
+                                    result= prediction)
+        
+
+if __name__ == '__main__':
+    app.run()
